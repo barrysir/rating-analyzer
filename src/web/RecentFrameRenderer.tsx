9173d0635264f6bdbf1aa337e28c53d690585609
barrysir <14061419+barrysir@users.noreply.github.com>
2025-11-24 05:38:15 +0800

    Add song information to recent frame display

--- src/web/RecentFrameRenderer.tsx
+++ src/web/RecentFrameRenderer.tsx
@@ -1,20 +1,21 @@
 import { For } from 'solid-js';
+import { history } from './stores/historyStore';
 import { AgeFrameSnapshot } from '../rating/frames/AgeFrame';
+import { HistoricalChartDb } from '../rating/chartdb/HistoricalChartDb';
 
 export function RecentFrameRenderer<Score extends {points: number, rating: number}>(props: {snapshot: AgeFrameSnapshot<Score>}) {
   return <div style="display: flex; flex-direction: column; font-size: 0.8em; border: 1px solid black; border-radius: 4px;">
     <For each={props.snapshot.byRating}>
       {(item, index) => {
-        return <span>{item.rating} - {item.points}</span>
-        // let db: HistoricalChartDb = history.history!.calc.db;
-        // let key = db.parseChartId(item.id);
-        // let a = db.findChart(key);
-        // if (a === null) {
-        //   return null;
-        // }
-        // let {song, chart, difficulty} = a;
+        let db: HistoricalChartDb = history.history!.calc.db;
+        let key = db.parseChartId(item.id);
+        let a = db.findChart(key);
+        if (a === null) {
+          return null;
+        }
+        let {song, chart, difficulty} = a;
 
-        // return <span>{item.score.rating} {item.score.points} {song.artist} - {song.title} ({difficulty} {chart.level})</span>
+        return <span>{item.score.rating} {item.score.points} {song.artist} - {song.title} ({difficulty} {chart.level})</span>
       }}
     </For>  
   </div>
