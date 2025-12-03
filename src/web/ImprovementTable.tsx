92af109b11720da9eac81bd2d484c7b240782c45
barrysir <14061419+barrysir@users.noreply.github.com>
2025-12-02 17:34:28 -0800

    Change ImprovementTable to Accordion

--- src/web/ImprovementTable.tsx
+++ src/web/ImprovementTable.tsx
@@ -1,57 +1,61 @@
-import { Collapsible, MenuItem } from "@ark-ui/solid";
+import { Accordion, Collapsible, MenuItem } from "@ark-ui/solid";
 import { VersionImproveRenderData } from "./Temp";
-import { For, Show } from "solid-js";
+import { For, Index, Show } from "solid-js";
 import { historyGetDb, historyGetScore, historyGetSong } from "./stores/historyStore";
 import './ImprovementTable.css';
+import { Icon } from "@iconify-icon/solid";
 
-function FrameImprovementRender(props: {rating: number, change?: number, color: string}) {
-    return <div style={{'color': props.color, 'display': 'flex', 'flex-direction': 'column', 'align-items': 'center'}}>
+function FrameImprovementRender(props: { rating: number, change?: number, color: string }) {
+    return <div style={{ 'color': props.color, 'display': 'flex', 'flex-direction': 'column', 'align-items': 'center' }}>
         <span>{props.rating.toFixed(3)}</span>
         <Show when={props.change !== undefined}>
             <span style="font-size: 0.7em">{props.change.toFixed(3)}</span>
         </Show>
     </div>
 }
-function VersionImprovement(props: {data: VersionImproveRenderData}) {
+function VersionImprovement(props: { data: VersionImproveRenderData }) {
     // (point index) Version {} (center aligned?)       (different colour to signify it's collapsible)
     // (point index) {song title} - {points}        {rating} {best} {new} {recent} (tiny text below showing the increase to rating)
-    
-    return <Collapsible.Root>
-        <Collapsible.Trigger>
-            {props.data.pointId}
-            {/* <Collapsible.Indicator>
-        <ChevronRightIcon />
-      </Collapsible.Indicator> */}
-        </Collapsible.Trigger>
-        <Collapsible.Content>
-            <div class="improve-list">
-                <For each={props.data.improves}>
-                    {(item, index) => {
-                        let score = historyGetScore(item.scoreId)!;
-                        let song = historyGetSong(item.pointId, score.chartId); // Db(item.pointId).getChart(score.chartId).song;
-                        if (song === undefined) {
-                            console.warn("Couldn't find song info for", score.chartId, "at", item.pointId)
-                        }
-                        let frame = item.data;
-                        return <div class="improve-row">
-                            <span>{item.pointId}</span>
-                            <span>{song?.title}</span>
-                            <span>{score.points}</span>
-                            <span>{score.rating?.toFixed(2)}</span>
-                            <FrameImprovementRender rating={frame.total} change={frame.changes.total} color="black" />
-                            <FrameImprovementRender rating={frame.best} change={frame.changes.best} color="blue" />
-                            <FrameImprovementRender rating={frame.new} change={frame.changes.new} color="green" />
-                            <FrameImprovementRender rating={frame.recent} change={frame.changes.recent} color="red" />
-                        </div>
-                    }}
-                </For>
-            </div>
-        </Collapsible.Content>
-    </Collapsible.Root>
+
+    return <div class="improve-list">
+        <For each={props.data.improves}>
+            {(item, index) => {
+                let score = historyGetScore(item.scoreId)!;
+                let song = historyGetSong(item.pointId, score.chartId); // Db(item.pointId).getChart(score.chartId).song;
+                if (song === undefined) {
+                    console.warn("Couldn't find song info for", score.chartId, "at", item.pointId)
+                }
+                let frame = item.data;
+                return <div class="improve-row">
+                    <span>{item.pointId}</span>
+                    <span>{song?.title}</span>
+                    <span>{score.points}</span>
+                    <span>{score.rating?.toFixed(2)}</span>
+                    <FrameImprovementRender rating={frame.total} change={frame.changes.total} color="black" />
+                    <FrameImprovementRender rating={frame.best} change={frame.changes.best} color="blue" />
+                    <FrameImprovementRender rating={frame.new} change={frame.changes.new} color="green" />
+                    <FrameImprovementRender rating={frame.recent} change={frame.changes.recent} color="red" />
+                </div>
+            }}
+        </For>
+    </div>
 }
 
-export function ImprovementTable(props: { improves: any }) {
-    return <For each={props.improves}>
-        {(item, index) => <VersionImprovement data={item}/>}
-    </For>
+export function ImprovementTable(props: { improves: VersionImproveRenderData[] }) {
+    return <Accordion.Root multiple collapsible class="improvement-accordion">
+        <Index each={props.improves}>
+            {(item, index) => <Accordion.Item value={index.toString()} class="accordion-item">
+                <Accordion.ItemTrigger class="accordion-trigger">
+                    <span class="accordion-title">Version {item().pointId}</span>
+                    <Accordion.ItemIndicator class="accordion-indicator">
+                        <Icon icon="lucide:chevron-down" />
+                    </Accordion.ItemIndicator>
+                </Accordion.ItemTrigger>
+                <Accordion.ItemContent class="accordion-content">
+                    <VersionImprovement data={item()} />
+                </Accordion.ItemContent>
+            </Accordion.Item>
+            }
+        </Index>
+    </Accordion.Root>
 }
